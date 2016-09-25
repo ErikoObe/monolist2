60d33e1676b9cb5803a8e29a0a0fc3db70afc69c
Eriko Obe <eriko.obe@gmail.com>
2016-09-25 04:46:12 +0000

    challenge2_2

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -26,7 +26,12 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
-
+    if params[:type] == "Have"
+      current_user.have(@item)
+    elsif params[:type] == "Want"
+      current_user.want(@item)
+    end
+    
   end
 
   def destroy
@@ -35,6 +40,12 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+
+    if params[:type] == "Have it"
+      current_user.unhave(@item)
+    elsif params[:type] == "Want it"
+      current_user.unwant(@item)
+    end
 
   end
 end
